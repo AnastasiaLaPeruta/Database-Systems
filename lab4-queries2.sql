f97d40879e6565bf076e94f4595d8f4350152bce
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2025-02-24 13:39:00 -0500

    Neatly formatted and ready to start

---
+++ lab4-queries2.sql
@@ -0,0 +1,84 @@
+-- ANASTASIA LAPERUTA, 2/24/2025, LAB4-QUERIES2
+
+
+-- 1. my answer
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+
+-- 2. my answer
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 3. my answer
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 4. my answer
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 5. my answer
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 6. my answer
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 7. my answer
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 8. my answer
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 9. my answer
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 10. my answer
+
+
+-- Difference: ; AI answer /10
