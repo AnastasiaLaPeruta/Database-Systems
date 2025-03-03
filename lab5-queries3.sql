9a4803cfc96dc0111e56a47006de24fb3f8728a5
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2025-03-03 18:21:11 -0500

    set up neat outline for lab 5

---
+++ lab5-queries3.sql
@@ -0,0 +1,100 @@
+-- ANASTASIA LAPERUTA, 3/3/2025, LAB5-QUERIES3
+
+
+-- 1. my answer
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+
+-- 2. my answer
+ 
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 3. my answer
+ 
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 4. my answer
+
+
+-- Difference:; AI answer /10
+
+
+
+
+
+
+-- 5. my answer
+
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+
+-- 6. my answer
+
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+-- 7. my answer
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+
+-- 8. my answer
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+
+-- 9. my answer
+
+
+
+-- Difference: ; AI answer /10
+
+
+
+
+
+
+-- 10. my answer
+
+
+-- Difference: ; AI answer /10
+
+
