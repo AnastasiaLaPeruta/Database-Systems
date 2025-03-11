fa06c0a1f577fd0a92756702a87b426ecbb24407
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2025-03-11 09:41:36 -0400

    set up lab format

---
+++ lab6-queries4.sql
@@ -0,0 +1,111 @@
+-- ANASTASIA LAPERUTA, 3/11/2025, LAB6-QUERIES4
+
+
+-- 1. my answer
+
+
+
+-- Difference:; AI answer /10
+
+
+
+
+
+
+
+-- 2. my answer
+ 
+
+
+-- Difference:; AI answer /10
+
+
+
+
+
+-- 3. my answer
+ 
+
+-- Difference:; AI answer /10
+
+
+
+
+
+
+-- 4. my answer
+
+
+-- Difference:; AI answer /10
+
+
+
+
+
+
+
+
+-- 5. my answer
+
+
+
+-- Difference:; AI answer /10
+
+
+
+
+
+
+
+-- 6. my answer
+
+
+
+
+-- Difference:; AI answer /10
+
+
+
+
+
+-- 7. my answer
+
+
+-- Difference:; AI answer /10
+
+
+
+
+
+-- 8. my answer
+
+
+
+-- Difference:; AI answer /10
+
+
+
+
+
+
+-- 9. my answer
+
+
+
+
+-- Difference:; AI answer /10
+
+
+
+
+
+
+
+
+-- 10. my answer
+
+
+-- Difference:; AI answer /10
+
+
+
